326c05b0b92b1ab3a46488d1cefe1678157efd3e
Luis Felipe Guerreiro Ota <161407693+filipeluisgg@users.noreply.github.com>
2025-03-03 03:20:56 +0000

    Add `database.js` and `.env` files.

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 2 + 93");
+  console.log(result);
   response.status(200).json({ chave: "Eu sou acima da média!" });
 }
 
