06e3468ddb79d72bfb7aeca69549945f1e4c8c8c
Luis Felipe Guerreiro Ota <filipeluisgg@gmail.com>
2025-04-04 20:00:48 -0300

    Improved prettier configurations.

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,38 +2,35 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  //Instanciar data para mostrar o momento da requisição.
-  const updatedAt = new Date().toISOString();
+	//Instanciar data para mostrar o momento da requisição.
+	const updatedAt = new Date().toISOString();
 
-  //Consultar versão do banco de dados.
-  const databaseVersionResult = await database.query("SHOW server_version;");
-  const databaseVersionValue = databaseVersionResult[0].server_version;
+	//Consultar versão do banco de dados.
+	const databaseVersionResult = await database.query("SHOW server_version;");
+	const databaseVersionValue = databaseVersionResult[0].server_version;
 
-  //Consultar máximo de conexões.
-  const databaseMaxConnectionsResult = await database.query(
-    "SHOW max_connections;",
-  );
-  const databaseMaxConnectionsValue =
-    databaseMaxConnectionsResult[0].max_connections;
+	//Consultar máximo de conexões.
+	const databaseMaxConnectionsResult = await database.query("SHOW max_connections;");
+	const databaseMaxConnectionsValue = databaseMaxConnectionsResult[0].max_connections;
 
-  //Consultar número de conexões abertas.
-  const databaseName = process.env.POSTGRES_DB;
-  const openedConnectionsResult = await database.query({
-    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
-    values: [databaseName],
-  });
-  const openedConnectionsValue = openedConnectionsResult[0].count;
+	//Consultar número de conexões abertas.
+	const databaseName = process.env.POSTGRES_DB;
+	const openedConnectionsResult = await database.query({
+		text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+		values: [databaseName],
+	});
+	const openedConnectionsValue = openedConnectionsResult[0].count;
 
-  response.status(200).json({
-    updated_at: updatedAt,
-    dependencies: {
-      database: {
-        max_connections: parseInt(databaseMaxConnectionsValue),
-        opened_connections: openedConnectionsValue,
-        version: databaseVersionValue,
-      },
-    },
-  });
+	response.status(200).json({
+		updated_at: updatedAt,
+		dependencies: {
+			database: {
+				max_connections: parseInt(databaseMaxConnectionsValue),
+				opened_connections: openedConnectionsValue,
+				version: databaseVersionValue,
+			},
+		},
+	});
 }
 
 export default status;
--- pages/index.js
+++ pages/index.js
@@ -1,66 +1,62 @@
 function Home() {
-  return (
-    <>
-      <div className="container">
-        <h1>
-          <code>
-            A ideia é que o <b>nationnews.com.br</b> se torne um espaço único,
-            onde o conhecimento sobre países é compartilhado de forma
-            colaborativa e democrática, contribuindo para a informação de
-            qualidade e o debate construtivo. Os usuários que frequentarem a
-            plataforma serão, em sua maioria, pessoas curiosas, engajadas e
-            dispostas a participar ativamente de discussões que valorizam a
-            diversidade e o aprofundamento cultural e político.
-          </code>
-        </h1>
-      </div>
-      <style jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-        }
+	return (
+		<>
+			<div className="container">
+				<h1>
+					<code>
+						A ideia é que o <b>nationnews.com.br</b> se torne um espaço único, onde o conhecimento sobre países é compartilhado de forma colaborativa e democrática, contribuindo para a
+						informação de qualidade e o debate construtivo. Os usuários que frequentarem a plataforma serão, em sua maioria, pessoas curiosas, engajadas e dispostas a participar ativamente
+						de discussões que valorizam a diversidade e o aprofundamento cultural e político.
+					</code>
+				</h1>
+			</div>
+			<style jsx>{`
+				* {
+					margin: 0;
+					padding: 0;
+					box-sizing: border-box;
+				}
 
-        html,
-        body {
-          height: 100%;
-          width: 100%;
-          overflow: hidden;
-          font-family: "Helvetica", sans-serif;
-        }
+				html,
+				body {
+					height: 100%;
+					width: 100%;
+					overflow: hidden;
+					font-family: "Helvetica", sans-serif;
+				}
 
-        .container {
-          position: fixed; /* Mantém a div fixa na tela */
-          top: 0;
-          left: 0;
-          width: 100vw;
-          height: 100vh;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          background-color: #1e1e2e;
-        }
+				.container {
+					position: fixed; /* Mantém a div fixa na tela */
+					top: 0;
+					left: 0;
+					width: 100vw;
+					height: 100vh;
+					display: flex;
+					justify-content: center;
+					align-items: center;
+					background-color: #1e1e2e;
+				}
 
-        h1 {
-          color: white;
-          background-color: #282843;
-          padding: 20px;
-          border-radius: 10px;
-          text-align: center;
-          max-width: 90%; /* Limita a largura para evitar que fique muito grande */
-          word-wrap: break-word; /* Garante que o texto quebre corretamente */
-        }
+				h1 {
+					color: white;
+					background-color: #282843;
+					padding: 20px;
+					border-radius: 10px;
+					text-align: center;
+					max-width: 90%; /* Limita a largura para evitar que fique muito grande */
+					word-wrap: break-word; /* Garante que o texto quebre corretamente */
+				}
 
-        code {
-          color: lightblue;
-        }
+				code {
+					color: lightblue;
+				}
 
-        b {
-          color: blue;
-        }
-      `}</style>
-    </>
-  );
+				b {
+					color: blue;
+				}
+			`}</style>
+		</>
+	);
 }
 
 export default Home;
