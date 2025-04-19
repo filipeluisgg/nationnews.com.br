75ee3165715b2f1b2e9840b0cee85435633f61d9
Luis Felipe <filipeluisgg@gmail.com>
2025-04-19 08:52:50 -0300

    Formated `post.test.js`

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -19,6 +19,5 @@ test("POST to api/v1/migrations should return 200", async () => {
 
 	const response2Body = await response2.json();
 	expect(Array.isArray(response2Body)).toBe(true);
-
 	expect(response2Body.length).toBe(0);
 });
