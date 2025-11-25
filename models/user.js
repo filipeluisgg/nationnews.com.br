edf0a27f184cc948b6a4f9a05e241b2047ef3841
Luis Felipe Ota <filipeluisgg+github@gmail.com>
2025-11-25 12:55:01 -0300

    feat: implement `/api/v1/user` endpoint with session renewal

--- models/user.js
+++ models/user.js
@@ -2,6 +2,39 @@ import database from "infra/database.js";
 import password from "models/password.js";
 import { ValidationError, NotFoundError } from "infra/errors.js";
 
+async function findOneById(id) {
+	const userFound = await runSelectQuery(id);
+
+	return userFound;
+
+	async function runSelectQuery(id) {
+		const results = await database.query({
+			text: ` 
+                SELECT 
+                    * 
+                FROM 
+                    users 
+                WHERE 
+                    id = $1
+                LIMIT 
+                    1
+                ;`,
+			values: [id],
+		});
+
+		if (results.rowCount === 0) {
+			throw new NotFoundError({
+				name: "NotFoundError",
+				message: "O id informado não foi encontrado no sistema.",
+				action: "Verifique se o id está digitado corretamente.",
+				status_code: 404,
+			});
+		}
+
+		return results.rows[0];
+	}
+}
+
 async function findOneByUsername(username) {
 	const userFound = await runSelectQuery(username);
 
@@ -187,6 +220,12 @@ async function hashPasswordInObject(userInputValues) {
 	userInputValues.password = hashedPassword;
 }
 
-const user = { create, findOneByUsername, findOneByEmail, update };
+const user = {
+	create,
+	findOneById,
+	findOneByUsername,
+	findOneByEmail,
+	update,
+};
 
 export default user;
