15431c6ee8493276c77e04a6858844acc5015318
Luis Felipe Ota <filipeluisgg+github@gmail.com>
2025-12-06 22:10:05 -0300

    feat: add `PATCH` `/api/v1/activation/[token_id]`

--- models/activation.js
+++ models/activation.js
@@ -2,6 +2,7 @@ import database from "infra/database.js";
 import webserver from "infra/webserver.js";
 import { NotFoundError } from "infra/errors.js";
 import email from "infra/email.js";
+import user from "models/user.js";
 
 const FIFTEEN_MINUTES_IN_MILLISECONDS = 60 * 15 * 1000;
 
@@ -60,6 +61,35 @@ async function create(userId) {
 	}
 }
 
+async function markTokenAsUsed(activationTokenId) {
+	const usedActivationToken = await runUpdateQuery(activationTokenId);
+	return usedActivationToken;
+
+	async function runUpdateQuery(activationTokenId) {
+		const results = await database.query({
+			text: `
+                UPDATE
+                    user_activation_tokens
+                SET
+                    used_at = timezone('utc', now()),
+                    updated_at = timezone('utc', now())
+                WHERE
+                    id = $1
+                RETURNING
+                    *
+            ;`,
+			values: [activationTokenId],
+		});
+
+		return results.rows[0];
+	}
+}
+
+async function activateUserByUserId(userId) {
+	const activatedUser = await user.setFeatures(userId, ["create:session"]);
+	return activatedUser;
+}
+
 async function sendEmailToUser(user, activationToken) {
 	await email.send({
 		from: "NationNews <contato@nationnews.com.br>",
@@ -77,7 +107,9 @@ Equipe NationNews.`,
 const activation = {
 	create,
 	sendEmailToUser,
+	markTokenAsUsed,
 	findOneValidById,
+	activateUserByUserId,
 };
 
 export default activation;
