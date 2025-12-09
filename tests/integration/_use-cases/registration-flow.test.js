2b6202c5902a3515d7e4eee5e0fbc0d282580a6f
Luis Felipe Ota <filipeluisgg+github@gmail.com>
2025-12-09 19:41:43 -0300

    feat: add `injectAnonymousOrUser` and `canRequest` middlewares to `/sessions`

--- tests/integration/_use-cases/registration-flow.test.js
+++ tests/integration/_use-cases/registration-flow.test.js
@@ -10,7 +10,7 @@ beforeAll(async () => {
 	await orchestrator.deleteAllEmails();
 });
 
-describe("Use case: Registration Flow (All successful", () => {
+describe("Use case: Registration Flow (All successful)", () => {
 	let createUserResponseBody;
 	let activationTokenId;
 
@@ -78,7 +78,24 @@ describe("Use case: Registration Flow (All successful", () => {
 		expect(activatedUser.features).toEqual(["create:session"]);
 	});
 
-	test("Login", async () => {});
+	test("Login", async () => {
+		const createSessionResponse = await fetch("http://localhost:3000/api/v1/sessions", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				email: "registration.flow@email.com.br",
+				password: "RegistrationFlowPassword",
+			}),
+		});
+
+		expect(createSessionResponse.status).toBe(201);
+
+		const createSessionResponseBody = await createSessionResponse.json();
+
+		expect(createSessionResponseBody.user_id).toBe(createUserResponseBody.id);
+	});
 
 	test("Get user information", async () => {});
 });
