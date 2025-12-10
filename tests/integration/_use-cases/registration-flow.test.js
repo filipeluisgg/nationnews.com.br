c560b7b026521548bcb8f766b71755716cb2b5d0
Luis Felipe Ota <filipeluisgg+github@gmail.com>
2025-12-10 00:32:34 -0300

    feat: require `read:session` to acces `/user` endpoint

--- tests/integration/_use-cases/registration-flow.test.js
+++ tests/integration/_use-cases/registration-flow.test.js
@@ -13,6 +13,7 @@ beforeAll(async () => {
 describe("Use case: Registration Flow (All successful)", () => {
 	let createUserResponseBody;
 	let activationTokenId;
+	let createSessionsResponseBody;
 
 	test("Create user account", async () => {
 		const createUserResponse = await fetch("http://localhost:3000/api/v1/users", {
@@ -75,11 +76,11 @@ describe("Use case: Registration Flow (All successful)", () => {
 		expect(Date.parse(activationResponseBody.used_at)).not.toBeNaN();
 
 		const activatedUser = await user.findOneByUsername("RegistrationFlow");
-		expect(activatedUser.features).toEqual(["create:session"]);
+		expect(activatedUser.features).toEqual(["create:session", "read:session"]);
 	});
 
 	test("Login", async () => {
-		const createSessionResponse = await fetch("http://localhost:3000/api/v1/sessions", {
+		const createSessionsResponse = await fetch("http://localhost:3000/api/v1/sessions", {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json",
@@ -90,12 +91,24 @@ describe("Use case: Registration Flow (All successful)", () => {
 			}),
 		});
 
-		expect(createSessionResponse.status).toBe(201);
+		expect(createSessionsResponse.status).toBe(201);
 
-		const createSessionResponseBody = await createSessionResponse.json();
+		createSessionsResponseBody = await createSessionsResponse.json();
 
-		expect(createSessionResponseBody.user_id).toBe(createUserResponseBody.id);
+		expect(createSessionsResponseBody.user_id).toBe(createUserResponseBody.id);
 	});
 
-	test("Get user information", async () => {});
+	test("Get user information", async () => {
+		const userResponse = await fetch("http://localhost:3000/api/v1/user", {
+			headers: {
+				cookie: `session_id=${createSessionsResponseBody.token}`,
+			},
+		});
+
+		expect(userResponse.status).toBe(200);
+
+		const userResponseBody = await userResponse.json();
+
+		expect(userResponseBody.id).toBe(createUserResponseBody.id);
+	});
 });
--- tests/integration/api/v1/user/get.test.js
+++ tests/integration/api/v1/user/get.test.js
@@ -10,11 +10,28 @@ beforeAll(async () => {
 });
 
 describe("GET /api/v1/user", () => {
+	describe("Anonymous user", () => {
+		test("Retrieving the endpoint", async () => {
+			const response = await fetch("http://localhost:3000/api/v1/user");
+
+			const responseBody = await response.json();
+
+			expect(responseBody).toEqual({
+				name: "ForbiddenError",
+				message: "Você não possui permissão para executar esta ação.",
+				action: 'Verifique se o seu usuário possui a feature "read:session"',
+				status_code: 403,
+			});
+		});
+	});
+
 	describe("Default user", () => {
 		test("With valid session", async () => {
 			const createdUser = await orchestrator.createUser({
 				username: "UserWithValidSession",
 			});
+
+			const activatedUser = await orchestrator.activateUser(createdUser);
 
 			const sessionObject = await orchestrator.createSession(createdUser.id);
 
@@ -35,10 +52,10 @@ describe("GET /api/v1/user", () => {
 				id: createdUser.id,
 				username: "UserWithValidSession",
 				email: createdUser.email,
-				features: ["read:activation_token"],
+				features: ["create:session", "read:session"],
 				password: createdUser.password,
 				created_at: createdUser.created_at.toISOString(),
-				updated_at: createdUser.updated_at.toISOString(),
+				updated_at: activatedUser.updated_at.toISOString(),
 			});
 
 			expect(uuidVersion(responseBody.id)).toBe(4);
@@ -72,6 +89,8 @@ describe("GET /api/v1/user", () => {
 				username: "UserHalfwayExpiredSession",
 			});
 
+			const activatedUser = await orchestrator.activateUser(createdUser);
+
 			const sessionObject = await orchestrator.createSession(createdUser.id);
 
 			jest.useRealTimers();
@@ -90,10 +109,10 @@ describe("GET /api/v1/user", () => {
 				id: createdUser.id,
 				username: "UserHalfwayExpiredSession",
 				email: createdUser.email,
-				features: ["read:activation_token"],
+				features: ["create:session", "read:session"],
 				password: createdUser.password,
 				created_at: createdUser.created_at.toISOString(),
-				updated_at: createdUser.updated_at.toISOString(),
+				updated_at: activatedUser.updated_at.toISOString(),
 			});
 
 			expect(uuidVersion(responseBody.id)).toBe(4);
