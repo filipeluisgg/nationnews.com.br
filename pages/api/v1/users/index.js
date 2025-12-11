8dbe1a0f96f21f82c363c0d7d51c5824cd57573d
Luis Felipe Ota <filipeluisgg+github@gmail.com>
2025-12-11 12:00:15 -0300

    feat: require `create:user` to access `/api/v1/users`

--- pages/api/v1/users/index.js
+++ pages/api/v1/users/index.js
@@ -5,7 +5,8 @@ import user from "models/user.js";
 
 const router = createRouter();
 
-router.post(postHandler);
+router.use(controller.injectAnonymousOrAuthenticatedUser);
+router.post(controller.canRequest("create:user"), postHandler);
 
 export default router.handler(controller.errorHandlers);
 
