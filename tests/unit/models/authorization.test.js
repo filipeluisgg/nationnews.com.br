edfeb628b0610b57a13d83fe780763be37050279
Luis Felipe Ota <filipeluisgg+github@gmail.com>
2026-05-22 21:47:47 -0300

    test: fix `ISO` date format in `authorization` unit tests

--- tests/unit/models/authorization.test.js
+++ tests/unit/models/authorization.test.js
@@ -70,8 +70,8 @@ describe("models/authorization.js", () => {
 				id: 1,
 				username: "resource",
 				features: ["read:user"],
-				created_at: "2026-0101T00:00:00:000Z",
-				updated_at: "2026-0101T00:00:00:000Z",
+				created_at: "2026-01-01T00:00:00:000Z",
+				updated_at: "2026-01-01T00:00:00:000Z",
 				email: "resource@resource.com",
 				password: "resource",
 			};
@@ -82,8 +82,8 @@ describe("models/authorization.js", () => {
 				id: 1,
 				username: "resource",
 				features: ["read:user"],
-				created_at: "2026-0101T00:00:00:000Z",
-				updated_at: "2026-0101T00:00:00:000Z",
+				created_at: "2026-01-01T00:00:00:000Z",
+				updated_at: "2026-01-01T00:00:00:000Z",
 			});
 		});
 	});
